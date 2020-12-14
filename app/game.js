f9b77a6f55bc666ce33838cc707ab9ea7c7fb1af
Laurent Muller <laurent.muller@bepatient.com>
2020-12-14 18:34:02 +0100

    master: Added buttons

--- app/game.js
+++ app/game.js
@@ -2,59 +2,138 @@
   "use strict";
   window.addEventListener("DOMContentLoaded", onDomContentLoaded);
 
+  let canvas = undefined;
+  let game = undefined;
+  let playerBall = undefined;
+  let raf = undefined;
+
+  let startButton = undefined;
+
   function onDomContentLoaded() {
-    const canvas = getCanvas();
+    canvas = getCanvas();
+    game = new Game();
+    setStartButton();
+
     if (Boolean(canvas)) {
       addListeners(canvas);
       draw(canvas);
     }
   }
 
+  function setStartButton() {
+    startButton = document.querySelector("#startGame");
+    startButton.onclick = onStartButtonClicked;
+  }
+
+  function onStartButtonClicked(evt) {
+    game.started = true;
+    console.log(game);
+  }
+
   function getCanvas() {
     return document.querySelector("#myGame");
   }
 
-  function addListeners(canvas) {
+  function addListeners() {
     const events = {
-      mouseOver: "mouseover",
-      mouseOut: "mouseout",
-      mouseLeave: "mouseleave",
+      keyDown: "keydown",
+      keyUp: "keyup",
     };
-    let raf = null;
 
-    canvas.addEventListener(events.mouseOver, onMouseOver);
-    canvas.addEventListener(events.mouseLeave, onMouseLeave);
+    window.addEventListener(events.keyDown, onKeyDown);
+    window.addEventListener(events.keyUp, onKeyUp);
 
-    function onMouseOver(evt) {
-      raf = window.requestAnimationFrame(animate);
+    function onKeyDown(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      movePlayerBall(evt);
     }
 
-    function onMouseLeave(evt) {
-      window.cancelAnimationFrame(raf);
+    function onKeyUp(evt) {
+      // TODO: implements key yup
     }
   }
 
-  function animate(evt) {
-    console.log(evt);
+  function movePlayerBall(evt) {
+    playerBall.move(evt);
+    animate();
+  }
+
+  function animate() {
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    playerBall.draw();
+    manageAnimationFrame(true);
   }
 
   function draw(canvas) {
     const context = canvas.getContext("2d");
-    const playerBall = new Ball({ context });
+    const color = getComputedStyle(document.documentElement).getPropertyValue(
+      "--primary"
+    );
+
+    playerBall = new Ball({ positionX: 100, color, positionY: 100, context });
+    game.playerBall = new Ball({
+      positionX: 100,
+      color,
+      positionY: 100,
+      context,
+    });
     playerBall.draw();
+  }
+
+  function manageAnimationFrame(started) {
+    if (started) {
+      raf = window.requestAnimationFrame(animate);
+    } else {
+      window.cancelAnimationFrame(raf);
+    }
   }
 })();
 
 class Ball {
-  constructor({ size = 25, color = "blue", context }) {
-    this.x = size;
-    this.y = size;
+  constructor({ positionX, positionY, color = "blue", context }) {
+    this.x = positionX;
+    this.y = positionY;
     this.context = context;
     this.color = color;
-    this.radius = size / 4;
+  }
+  speedX = 2;
+  speedY = 2;
+  radius = 4;
+  direction = undefined;
+  directions = {
+    left: "left",
+    right: "right",
+    up: "up",
+    down: "down",
+  };
+
+  setDirection(evt) {
+    const codes = {
+      left: "ArrowLeft",
+      right: "ArrowRight",
+      up: "ArrowUp",
+      down: "ArrowDown",
+    };
+
+    switch (evt.code) {
+      case codes.left:
+        return this.directions.left;
+
+      case codes.right:
+        return this.directions.right;
+
+      case codes.up:
+        return this.directions.up;
+
+      case codes.down:
+        return this.directions.down;
+
+      default:
+        return;
+    }
   }
 
-  speed = 2;
   draw() {
     this.context.beginPath();
     this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -62,4 +141,54 @@ class Ball {
     this.context.fillStyle = this.color;
     this.context.fill();
   }
+
+  animate() {
+    switch (this.direction) {
+      case this.directions.left:
+        this.x -= this.speedX;
+        break;
+
+      case this.directions.right:
+        this.x += this.speedX;
+        break;
+
+      case this.directions.up:
+        this.y -= this.speedY;
+        break;
+
+      case this.directions.down:
+        this.y += this.speedY;
+
+      default:
+        break;
+    }
+
+    // const minHeight = 0;
+    // const minWidth = 0;
+    // const maxHeight = this.context.canvas.clientHeight;
+    // const maxWidth = this.context.canvas.clientWidth;
+    // const nextPositionX = this.x + this.speedX;
+    // const nextPositionY = this.y + this.speedY;
+
+    // if (nextPositionX > maxWidth || nextPositionX < minWidth) {
+    //   this.speedX = -this.speedX;
+    // }
+    // if (nextPositionY > maxHeight || nextPositionY < minHeight) {
+    //   this.speedY = -this.speedY;
+    // }
+
+    // this.x += this.speedX;
+    // this.y += this.speedY;
+    this.draw();
+  }
+
+  move(evt) {
+    this.direction = this.setDirection(evt);
+    this.animate();
+  }
+}
+
+class Game {
+  started = false;
+  playerBall = {};
 }
