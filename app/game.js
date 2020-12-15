5f807f2354c065e45ade7dc5d4cdd3a31238baed
Laurent Muller <laurent.muller@bepatient.com>
2020-12-15 11:28:27 +0100

    master: Added animation

--- app/game.js
+++ app/game.js
@@ -3,21 +3,23 @@
   window.addEventListener("DOMContentLoaded", onDomContentLoaded);
 
   let canvas = undefined;
-  let game = undefined;
-  let playerBall = undefined;
+  let game = {};
   let raf = undefined;
 
   let startButton = undefined;
 
   function onDomContentLoaded() {
     canvas = getCanvas();
-    game = new Game();
     setStartButton();
 
     if (Boolean(canvas)) {
+      setGame(canvas);
       addListeners(canvas);
-      draw(canvas);
     }
+  }
+
+  function setGame(canvas) {
+    game = new Game({ canvas });
   }
 
   function setStartButton() {
@@ -25,9 +27,27 @@
     startButton.onclick = onStartButtonClicked;
   }
 
-  function onStartButtonClicked(evt) {
-    game.started = true;
-    console.log(game);
+  function onStartButtonClicked() {
+    game.switchState();
+    if (!game.started) {
+      manageAnimationFrame(game.started);
+    } else {
+      manageAnimationFrame(game.started);
+    }
+  }
+
+  function draw() {
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    game.animate();
+    manageAnimationFrame(game.started);
+  }
+
+  function manageAnimationFrame(started) {
+    if (started) {
+      raf = window.requestAnimationFrame(draw);
+    } else {
+      window.cancelAnimationFrame(raf);
+    }
   }
 
   function getCanvas() {
@@ -68,51 +88,59 @@
   }
 
   function movePlayerBall(evt) {
-    playerBall.move(evt);
-    animate();
-  }
-
-  function animate() {
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    playerBall.draw();
-    manageAnimationFrame(true);
-  }
-
-  function draw(canvas) {
-    const context = canvas.getContext("2d");
-    const color = getComputedStyle(document.documentElement).getPropertyValue(
-      "--primary"
-    );
-
-    playerBall = new Ball({ positionX: 100, color, positionY: 100, context });
-    game.playerBall = new Ball({
-      positionX: 100,
-      color,
-      positionY: 100,
-      context,
-    });
-    playerBall.draw();
-  }
-
-  function manageAnimationFrame(started) {
-    if (started) {
-      raf = window.requestAnimationFrame(animate);
-    } else {
-      window.cancelAnimationFrame(raf);
-    }
+    game.movePlayerBall(evt);
   }
 })();
 
 class Ball {
-  constructor({ positionX, positionY, color = "blue", context }) {
+  constructor({
+    positionX,
+    positionY,
+    color = "blue",
+    radius = 25,
+    speedX = 5,
+    speedY = 5,
+    context,
+  }) {
     this.x = positionX;
     this.y = positionY;
     this.context = context;
     this.color = color;
+    this.radius = radius;
+    this.speedX = speedX;
+    this.speedY = speedY;
   }
-  speedX = 2;
-  speedY = 2;
-  radius = 4;
+
+  draw() {
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    this.context.closePath();
+    this.context.fillStyle = this.color;
+    this.context.fill();
+  }
+
+  animate() {
+    const minHeight = 0;
+    const minWidth = 0;
+    const maxHeight = this.context.canvas.clientHeight;
+    const maxWidth = this.context.canvas.clientWidth;
+    const nextPositionX = this.x + this.speedX;
+    const nextPositionY = this.y + this.speedY;
+
+    if (nextPositionX > maxWidth || nextPositionX < minWidth) {
+      this.speedX = -this.speedX;
+    }
+    if (nextPositionY > maxHeight || nextPositionY < minHeight) {
+      this.speedY = -this.speedY;
+    }
+
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.draw();
+  }
+}
+
+class PlayerBall extends Ball {
   direction = undefined;
   directions = {
     left: "left",
@@ -121,7 +149,38 @@ class Ball {
     down: "down",
   };
 
-  setDirection(evt) {
+  move(evt) {
+    this.direction = this.getDirection(evt);
+    if (this.direction) {
+      this.animate();
+    }
+  }
+
+  animate() {
+    switch (this.direction) {
+      case this.directions.left:
+        this.x -= this.speedX;
+        break;
+
+      case this.directions.right:
+        this.x += this.speedX;
+        break;
+
+      case this.directions.up:
+        this.y -= this.speedY;
+        break;
+
+      case this.directions.down:
+        this.y += this.speedY;
+        break;
+
+      default:
+        break;
+    }
+    this.draw();
+  }
+
+  getDirection(evt) {
     const codes = {
       left: "ArrowLeft",
       right: "ArrowRight",
@@ -146,64 +205,66 @@ class Ball {
         return;
     }
   }
-
-  draw() {
-    this.context.beginPath();
-    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    this.context.closePath();
-    this.context.fillStyle = this.color;
-    this.context.fill();
-  }
-
-  animate() {
-    switch (this.direction) {
-      case this.directions.left:
-        this.x -= this.speedX;
-        break;
-
-      case this.directions.right:
-        this.x += this.speedX;
-        break;
-
-      case this.directions.up:
-        this.y -= this.speedY;
-        break;
-
-      case this.directions.down:
-        this.y += this.speedY;
-        break;
-
-      default:
-        break;
-    }
-
-    // const minHeight = 0;
-    // const minWidth = 0;
-    // const maxHeight = this.context.canvas.clientHeight;
-    // const maxWidth = this.context.canvas.clientWidth;
-    // const nextPositionX = this.x + this.speedX;
-    // const nextPositionY = this.y + this.speedY;
-
-    // if (nextPositionX > maxWidth || nextPositionX < minWidth) {
-    //   this.speedX = -this.speedX;
-    // }
-    // if (nextPositionY > maxHeight || nextPositionY < minHeight) {
-    //   this.speedY = -this.speedY;
-    // }
-
-    // this.x += this.speedX;
-    // this.y += this.speedY;
-    this.draw();
-  }
-
-  move(evt) {
-    this.direction = this.setDirection(evt);
-    this.animate();
-  }
 }
 
 class Game {
+  constructor({ canvas }) {
+    this.canvas = canvas;
+    this.context = canvas.getContext("2d");
+    this.createEnemyBall();
+    this.createPlayerBall();
+  }
+  color = getComputedStyle(document.documentElement).getPropertyValue(
+    "--primary"
+  );
+
   started = false;
   enemyBalls = [];
   playerBall = {};
+
+  switchState() {
+    this.started = !this.started;
+    this.animate();
+    // this.enemyBalls.forEach((ball) => ball.animate());
+  }
+
+  createPlayerBall() {
+    const playerBallOptions = {
+      positionX: this.canvas.width / 2,
+      positionY: this.canvas.height / 2,
+      color: this.color,
+      context: this.context,
+    };
+    this.playerBall = new PlayerBall(playerBallOptions);
+  }
+
+  createEnemyBall() {
+    const ballOptions = {
+      positionX: 100,
+      positionY: 100,
+      color: "red",
+      context: this.context,
+    };
+    const ball2Options = {
+      positionX: 50,
+      positionY: 10,
+      radius: 5,
+      color: "green",
+      context: this.context,
+    };
+    const ball = new Ball(ballOptions);
+    const ball2 = new Ball(ball2Options);
+    this.enemyBalls = [...this.enemyBalls, ball, ball2];
+  }
+
+  animate() {
+    this.enemyBalls.forEach((ball) => ball.animate());
+    this.playerBall.animate();
+  }
+
+  movePlayerBall(evt) {
+    if (this.started) {
+      this.playerBall.move(evt);
+    }
+  }
 }
